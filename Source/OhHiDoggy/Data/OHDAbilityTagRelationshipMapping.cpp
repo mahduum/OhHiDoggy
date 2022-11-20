42bd127158eed88220e465f12e3bb4078c79b6ad
Mahduum <mahduum@gmail.com>
2022-11-20 13:01:36 +0100

    Added tag relationship mapping.

---
+++ Source/OhHiDoggy/Data/OHDAbilityTagRelationshipMapping.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OHDAbilityTagRelationshipMapping.h"
+
+
+void UOHDAbilityTagRelationshipMapping::GetAbilityTagsToBlockAndCancel(const FGameplayTagContainer& AbilityTags, FGameplayTagContainer* OutTagsToBlock, FGameplayTagContainer* OutTagsToCancel) const
+{
+	// Simple iteration for now
+	for (int32 i = 0; i < AbilityTagRelationships.Num(); i++)
+	{
+		const FOHDAbilityTagRelationship& Tags = AbilityTagRelationships[i];
+		if (AbilityTags.HasTag(Tags.AbilityTag))
+		{
+			if (OutTagsToBlock)
+			{
+				OutTagsToBlock->AppendTags(Tags.AbilityTagsToBlock);
+			}
+			if (OutTagsToCancel)
+			{
+				OutTagsToCancel->AppendTags(Tags.AbilityTagsToCancel);
+			}
+		}
+	}
+}
+
+void UOHDAbilityTagRelationshipMapping::GetRequiredAndBlockedActivationTags(const FGameplayTagContainer& AbilityTags, FGameplayTagContainer* OutActivationRequired, FGameplayTagContainer* OutActivationBlocked) const
+{
+	// Simple iteration for now
+	for (int32 i = 0; i < AbilityTagRelationships.Num(); i++)
+	{
+		const FOHDAbilityTagRelationship& Tags = AbilityTagRelationships[i];
+		if (AbilityTags.HasTag(Tags.AbilityTag))
+		{
+			if (OutActivationRequired)
+			{
+				OutActivationRequired->AppendTags(Tags.ActivationRequiredTags);
+			}
+			if (OutActivationBlocked)
+			{
+				OutActivationBlocked->AppendTags(Tags.ActivationBlockedTags);
+			}
+		}
+	}
+}
+
+bool UOHDAbilityTagRelationshipMapping::IsAbilityCancelledByTag(const FGameplayTagContainer& AbilityTags, const FGameplayTag& ActionTag) const
+{
+	// Simple iteration for now
+	for (int32 i = 0; i < AbilityTagRelationships.Num(); i++)
+	{
+		const FOHDAbilityTagRelationship& Tags = AbilityTagRelationships[i];
+
+		if (Tags.AbilityTag == ActionTag && Tags.AbilityTagsToCancel.HasAny(AbilityTags))
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
--- Source/OhHiDoggy/Data/OhHiDoggyPawnData.h
+++ Source/OhHiDoggy/Data/OhHiDoggyPawnData.h
@@ -6,13 +6,14 @@
 #include "Engine/DataAsset.h"
 #include "OhHiDoggyPawnData.generated.h"
 
+class UOHDAbilityTagRelationshipMapping;
 class UDoggyInputConfig;
-/**
+/*
  * UOhHiDoggyPawnData
  *
  * Non-mutable data asset that contains properties used to define a pawn.
  */
-UCLASS(BlueprintType, Const, Meta = (DisplayName = "Lyra Pawn Data", ShortTooltip = "Data asset used to define a Pawn."))
+UCLASS(BlueprintType, Const, Meta = (DisplayName = "OhHiDoggy Pawn Data", ShortTooltip = "Data asset used to define a Pawn."))
 class OHHIDOGGY_API UOhHiDoggyPawnData : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
@@ -30,17 +31,17 @@ public:
 
 	// Ability sets to grant to this pawn's ability system.
 	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "OhHiDoggy|Abilities")//todo make from primary data asset
-	// TArray<ULyraAbilitySet*> AbilitySets;
+	// TArray<ULyraAbilitySet*> AbilitySets;//todo abilities with data: ability class, ability level and ability input tag, gameplay effects and owner attribute sets
 
-	// What mapping of ability tags to use for actions taking by this pawn//todo make from udataasset (tags to cancel, tags required etc.)
-	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Lyra|Abilities")
-	// ULyraAbilityTagRelationshipMapping* TagRelationshipMapping;
+	// What mapping of ability tags to use for actions taking by this pawn//todo make from udataasset (tags to cancel, tags required, what other abilities is it blocking etc.)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "OhHiDoggy|Abilities")
+	UOHDAbilityTagRelationshipMapping* TagRelationshipMapping;
 
 	// Input configuration used by player controlled pawns to create input mappings and bind input actions.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "OhHiDoggy|Input")
 	UDoggyInputConfig* InputConfig;
 
 	// // Default camera mode used by player controlled pawns.
-	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Lyra|Camera")
-	// TSubclassOf<ULyraCameraMode> DefaultCameraMode;
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "OhHiDoggy|Camera")
+	// TSubclassOf<ULyraCameraMode> DefaultCameraMode;//TODO primary add camera modes
 };
